9b513e0003c3273830f71e5e57a0710b3b1d255d
pepe <github@pe82.de>
2020-02-24 20:42:32 +0100

    Change mouse click behaviour
    Left click will go to the next slide, right click will go back

--- presentation/js/reveal.js
+++ presentation/js/reveal.js
@@ -6023,6 +6023,16 @@
 		}
 	};
 
+window.addEventListener("mousedown", handleClick, false);
+window.addEventListener("contextmenu", function(e) { e.preventDefault(); }, false);
+
+function handleClick(e) {
+	e.preventDefault();
+	if(e.button === 0) Reveal.next(); 
+	if(e.button === 2) Reveal.prev(); 
+}
+
 	return Reveal;
 
 }));
+
